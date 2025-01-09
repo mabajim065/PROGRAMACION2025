8e6e46b147a17671bc678878fa5a0984f703bf60
mabajim065 <mariaabascal06@gmail.com>
2025-01-09 12:59:33 +0100

    9/1/25

--- unidad3/arma.java
+++ unidad3/arma.java
@@ -1,5 +1,58 @@
 package unidad3;
 
+/*
+	 * la clase arma sirve para guardar las caracteristicas de un  arma 
+	 * los podran llevar los personajes personajes del juego
+	 * 
+	 */
 public class arma {
+	/**
+	 * VARIABLES MIENBRO DE LA CLASE
+	 */
+
+	// es importante elegir algo que de miedo al enemigo
+	public String nombre = " ";
+	// descripcion del arma
+	public String descripcion = " arma por defecto , no hace pupa";
+	// efecto que lo infringe
+	int efecto = 0;
+
+	private int ataquefisico = 0;
+	private int ataquemagico = 0;
+	private int peso = 0;
+	private int clasepermitida[];
+
+	/**
+	 * CONSTRUCTORES DE LA CLASE
+	 * 
+	 * @return
+	 */
+	public arma() {
+		System.out.println("se ha creado un arma");
+		this.peso = (int) (Math.random() * 5);
+		this.ataquefisico = (int) (Math.random() * 18) + 2;
+		this.efecto = (int) (Math.random() * 5);
+
+	}
+
+	/**
+	 * GETTERS Y SETTERS DE LA CLASE
+	 * 
+	 * @return
+	 */
+
+	public String getNombre() {
+		return this.nombre;
+
+	}
+
+	public String setdescripcion() {
+		return this.setdescripcion();
+	}
+
+	public void setdescripcion(String valordescripcion) {
+		this.descripcion = valordescripcion;
+
+	}
 
 }
--- unidad3/ejemplousoobjetos.java
+++ unidad3/ejemplousoobjetos.java
@@ -13,11 +13,28 @@ public class ejemplousoobjetos {
 		 * puiede guardar datos
 		 */
 
+		/*
+		 * creamos un arma
+		 */
+		arma arcogelido = new arma();
+		// para poder usar un objeto primero hay que crearlo, se utiliza la palabra
+		// reseltada new, cuando lo creamos
+		/*
+		 * realmente se llama a una funcion de la clase arma que se denomina constructor
+		 */
+		arcogelido.nombre = "frozenlettheartgo";
+		String nombre;
 		personaje aragorn = new personaje();
 		aragorn.fuerza = 13;
 
 		System.out.println("el personaje tiene " + aragorn.inteligencia + "  puntos de inteligencia");
+		int golpe = 4;
+		aragorn.recibirdanio(golpe);
 		System.out.println("los puntos de vidas que tienes son: " + aragorn.puntosvida);
+		System.out.println("tirada de ataque aragon: " + ((Object) aragorn).tiradaataque());
+		System.out.println("descripcion del arma :" + arcogelido.getdescripcion());
+		System.out.println("descripcion del arma :" + arcogelido.getdescripcion());
+
 	}
 
 }
