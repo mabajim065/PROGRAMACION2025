76cb4439e1ac21337f6bbcfd64fc6e0374c679c8
mabajim065 <mariaabascal06@gmail.com>
2025-01-24 12:17:45 +0100

    24/01/25

---
+++ unidad3/enemigo.java
@@ -0,0 +1,23 @@
+package unidad3;
+
+public class enemigo extends personaje {// con esto ya tiene to las caracteristicas de personajes
+	private int pexperiencia;
+	private int dinero;
+	private String faccion;
+
+	public enemigo() {
+		// generamos la experiencia dpendiendo del ivel, cada nivle son 100 puntos extra
+		// de experiencia y un extra aleatorioq sera mas alto deendiendo del nivelñ
+		super();
+		int rangoexperiencia = this.getnivel() * 20;
+		this.pexperiencia = this.getnivel() * 100 + (int) (Math.random() * 20);
+		this.dinero = (int) (Math.random() * 20);
+		this.faccion = (int) (Math.random() * 20);
+	}
+
+	private int getnivel() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
