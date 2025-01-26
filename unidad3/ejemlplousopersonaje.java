6f7cefafa37ae310b103e8dd460d2b8b927b6862
MARÍA ABASCAL <mariaabascal06@gmail.com>
2025-01-26 13:15:00 +0100

    Update ejemlplousopersonaje.java

--- unidad3/ejemlplousopersonaje.java
+++ unidad3/ejemlplousopersonaje.java
@@ -1,45 +1,70 @@
 package unidad3;
 
-public class ejemlplousopersonaje {
+public class EjemploUsoPersonaje {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		/**
-		 * hemos creado una clase denominada personaje. las variables de una clase se
-		 * denominan objetos para crear un objeto utilizamos la palabra reservada new la
-		 * clase es el fichero java donde declaramos las variables y funciones que los
-		 * objetos pueden utilizar un objeto es como una variable de esa clase que
-		 * puiede guardar datos
-		 */
-
 		/*
-		 * creamos un arma
+		 * Hemos creado una clase denominada Personaje
+		 * Las variables de una clase se denominan objetos
+		 * Para crear un objeto utilizamos la palabra reservada new
+		 * La clase es el fichero java donde declaramos las variables y funciones que 
+		 * los objetos pueden utilizar
+		 * Un objeto es como una variable de esa clase que puede guardar datos
+		 * y ejecutar las funciones
 		 */
-		arma arcogelido = new arma();
-		// para poder usar un objeto primero hay que crearlo, se utiliza la palabra
-		// reseltada new, cuando lo creamos
-		/*
-		 * realmente se llama a una funcion de la clase arma que se denomina constructor
-		 */
-		arcogelido.nombre = "frozenlettheartgo";
-		String nombre;
-		personaje aragorn = new personaje();
-		aragorn.fuerza = 13;
+		//Creamos un arma
+		//Para poder usar un objeto primero hay que crearlo
+		//Se utiliza la palabra reserva new para indicar que lo creamos
+		//Cuando lo creamos realmente se llama a una funcion de la clase arma 
+		//que se denomina contructor
+		Arma arcoGelido = new Arma();
 
-		arma lanzapicas = new arma("lanza picas", "multiples a la vez", 0, 20, 0, 3);
+		Arma lanzaPicas = new Arma("lanza Picas", "multiples picas a la vez", 0, 20, 0, 3);
 
-		System.out.println("el personaje tiene " + aragorn.inteligencia + "  puntos de inteligencia");
+		String nombre = "pepe";
+
+		//Creamos un personaje con new y el nombre de la clase
+		Personaje aragorn = new Personaje();
+
+		aragorn.setClase(Personaje.CLASE_GUERRERO);
+
+		Enemigo slimeVerde = new Enemigo();
+
+		Enemigo orcoOscuro = new Enemigo(3, 23, 1, 5, 1, 23, 0, 350, 15, Enemigo.BOSQUE_OSCURO);
+
+		//añadimos las armas a aragorn
+		aragorn.addArma(arcoGelido);
+		aragorn.addArma(lanzaPicas);
+
+		//Para acceder a las variables miembro de la clase se utiliza el .
+		aragorn.setFuerza(13);
+
+		System.out.println("El personaje tiene " + aragorn.getInteligencia() + " puntos de inteligencia");
+
+		System.out.println("El arma principal de Aragorn es " + aragorn.armaPrincipal());
+
+		System.out.println("El personaje tiene " + aragorn.getPuntosVida() + " puntos de vida");
 		int golpe = 4;
-		aragorn.recibirdanio(golpe);
-		System.out.println("los puntos de vidas que tienes son: " + aragorn.puntosvida);
-		// System.out.println("tirada de ataque aragon: " + ((Object)
-		// aragorn).tiradaataque());
-		// System.out.println("descripcion del arma :" + arcogelido.getdescripcion());
-		// System.out.println("descripcion del arma :" + arcogelido.getdescripcion());
+		aragorn.recibirDanio(golpe);
 
-		System.out.println("el arma principal  de aragon es : " + aragorn.armaprincipal());
-		System.out.println("datos del slime:" + slimeverde);
+		System.out.println("El personaje tiene " + aragorn.getPuntosVida() + " puntos de vida");
+
+		System.out.println("Tirada de ataque de aragorn: " + aragorn.tiradaAtaque());
+
+		System.out.println("Descripcion del arma: " + arcoGelido.getDescripcion());
+
+		arcoGelido.setDescripcion("Congela los higadillos");
+
+		System.out.println("Descripcion del arma: " + arcoGelido.getDescripcion());
+
+		System.out.println("Datos del arma " + arcoGelido);
+
+		System.out.println("Datos del Slime " + slimeVerde);
+
+		System.out.println("Datos del Slime " + orcoOscuro);
+
 	}
 
 }
