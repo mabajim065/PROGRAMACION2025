2eb972a4bdd39f4c096fac1bab5c4dd497f6b80f
MARÍA ABASCAL <mariaabascal06@gmail.com>
2025-02-08 16:30:30 +0100

    Update Recuerdo.java

--- unidad3ejercicios/Recuerdo.java
+++ unidad3ejercicios/Recuerdo.java
@@ -1,108 +1,128 @@
-package unidad3ejercicios;
+package TEMA3;
 
-public class Recuerdo {
-
-	/******************************************************************************************************
-	 * VARIABLES ESTATICAS
-	 ******************************************************************************************************/
-	// Estas son las constantes que definen los rangos válidos para duración y
-	// fidelidad
-	public static final int MIN_DURACION = 1;
-	public static final int MAX_DURACION = 5;
-	public static final int MIN_FIDELIDAD = 1;
-	public static final int MAX_FIDELIDAD = 100;
-
-	/******************************************************************************************************
-	 * VARIABLES MIEMBROS
-	 ******************************************************************************************************/
-	private String nombre; // Aquí guardamos el nombre del recuerdo
-	private int duracion; // La duración del recuerdo, de 1 a 5 minutos
-	private int fidelidad; // La fidelidad, que es un porcentaje de 1 a 100
-
-	/******************************************************************************************************
-	 * CONSTRUCTORES
-	 ******************************************************************************************************/
-	// Constructor vacío. Esto lo usamos si no queremos inicializar nada específico,
-	// se llena con valores por defecto
-	public Recuerdo() {
-		this.nombre = ""; // Nombre vacío por defecto
-		this.duracion = 1; // Duración mínima
-		this.fidelidad = 50; // Fidelidad por defecto
-	}
-
-	// Constructor con parámetros. Aquí le pasamos los valores que queremos asignar
-	// al objeto
-	public Recuerdo(String nombre, int duracion, int fidelidad) {
-		this.nombre = nombre;
-		setDuracion(duracion); // Usamos setDuracion para asegurarnos que el valor esté dentro del rango
-		setFidelidad(fidelidad); // Igual con la fidelidad
-	}
-
-	/***********************************************************************************************************
-	 * METODOS Y FUNCIONES
-	 ***********************************************************************************************************/
-	// Este método comprueba si el recuerdo es "fiel". Si la fidelidad es baja, no
-	// es fiel.
-	// Dependiendo de la duración, tiene que tener una fidelidad mayor para ser
-	// considerado fiable
-	public boolean esFiel() {
-		if (fidelidad < 50)
-			return false; // Si la fidelidad es menor al 50%, no es fiel
-
-		switch (duracion) {
-		case 1:
-			return fidelidad > 90; // Si dura 1 minuto, la fidelidad debe ser mayor al 90%
-		case 2:
-			return fidelidad > 80; // Si dura 2 minutos, la fidelidad debe ser mayor al 80%
-		case 3:
-			return fidelidad > 70; // Si dura 3 minutos, la fidelidad debe ser mayor al 70%
-		case 4:
-			return fidelidad > 60; // Si dura 4 minutos, la fidelidad debe ser mayor al 60%
-		case 5:
-			return fidelidad > 50; // Si dura 5 minutos, la fidelidad debe ser mayor al 50%
-		default:
-			return false; // Si no se cumple ningún caso, no es fiel
+	public class Recuerdo {
+		/********************************************
+		 * 			VARIABLES ESTATICAS 			*
+		 *******************************************/
+		
+		
+		/********************************************
+		 * 			VARIABLES MIEMBRO 				*
+		 *******************************************/
+		//Creamos las variables miembro
+		private String nombre;
+		private int duracion;//Tiempo de 1-5min
+		private int fidelidad;//Porcentaje de 1 a 100
+		
+		/******************************************
+		 * CONSTRUCTORES
+		 ******************************************/
+		public Recuerdo() {
+			super();
+			this.nombre = "";
+			this.duracion = 0;
+			this.fidelidad = 0;
 		}
-	}
-
-	// El método toString muestra todos los datos del recuerdo de forma organizada
-	@Override
-	public String toString() {
-		return "Recuerdo: " + nombre + "\nDuración: " + duracion + " minutos\nFidelidad: " + fidelidad + "%";
-	}
-
-	/***************************************************************************************************************
-	 * GETTERS Y SETTERS
-	 ***************************************************************************************************************/
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public int getDuracion() {
-		return duracion;
-	}
-
-	public void setDuracion(int duracion) {
-		if (duracion >= MIN_DURACION && duracion <= MAX_DURACION) { // Validamos que esté entre 1 y 5
+		
+		public Recuerdo(String nombre, int duracion, int intesidad, int fidelidad) {
+			super();
+			this.nombre = nombre;
+			if(duracion>=1 || duracion <=5 )
 			this.duracion = duracion;
-		} else {
-			this.duracion = 1; // Si no es válido, ponemos 1 por defecto
-		}
-	}
-
-	public int getFidelidad() {
-		return fidelidad;
-	}
-
-	public void setFidelidad(int fidelidad) {
-		if (fidelidad >= MIN_FIDELIDAD && fidelidad <= MAX_FIDELIDAD) { // Validamos que sea entre 1 y 100
+			if(fidelidad >=1 || fidelidad <=100);
 			this.fidelidad = fidelidad;
-		} else {
-			this.fidelidad = 50; // Si no es válido, ponemos 50 por defecto
+		}
+		
+		/**************************************************************
+		 * GETTERS Y SETTERS DE LA CLASE
+		 *************************************************************/
+		
+		public String getNombre() {
+			return nombre;
+		}
+
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+		public int getDuracion() {
+			return duracion;
+		}
+
+		public void setDuracion(int duracion) {
+			this.duracion = duracion;
+		}
+
+
+		public int getFidelidad() {
+			return fidelidad;
+		}
+
+		public void setFidelidad(int fidelidad) {
+			this.fidelidad = fidelidad;
+		}
+		@Override
+		public String toString() {
+			return "Recuerdo: \nnombre=" + nombre + "\nduracion=" + duracion + ", \nintesidad=" + ", \nfidelidad="
+					+ fidelidad + "\n";
+		}
+
+		public static void main( String [] arg) {
+			Recuerdo b = new Recuerdo ("maripili", 10,6,4);
+			System.out.println(b);
+			
+		}
+		
+		/********************************************
+		 * 			METODOS O FUNCIONES 			*
+		 *******************************************/
+	//public boolean esFiel() : Si la fidelidad es inferior al 50% no es fiable y devuelve false. En caso contrario dependerá del tiempo del recuerdo, 
+	//para ser fiable deberá superar los valores de fidelidad de los siguientes datos:
+		//tiempo 1min=fidelidad>90
+		//tiempo 2min=fidelidad>80
+		//tiempo 3min=fidelidad>70
+		//tiempo 4min=fidelidad>60
+		//tiempo 5min=fidelidad>50
+
+		/*
+		 * otra forma= 
+		 * int[] rango = {90,80,70,60,50};
+		 * return fidelidad > rango[duracion -1];
+		 */
+		
+		public boolean esFiel() {
+			if (fidelidad < 50) {
+		        return false; // Si la fidelidad es menor a 50, no es fiable.
+		    }
+
+			if (duracion == 1) {
+					return fidelidad > 90;
+				} else if (duracion == 2) {
+					return fidelidad > 80;
+				} else if (duracion == 3) {
+					return fidelidad > 70;
+				} else if (duracion == 4) {
+					return fidelidad > 60;
+				} else if (duracion == 5) {
+					return fidelidad > 50;
+				}	
+			return false;
+			
+		}
+
+		
+		/**
+		 * necesito ambas funciones para que el usuario me borre si quiere algun recuerdo
+		 * @param indice
+		 */
+		public static void remove(int indice) { //esto se me tiene que cerar para que pueda borrarlos si quiero
+			// TODO Auto-generated method stub
+			
+		}
+
+		public static int size() { // igual que lo anterior
+			// TODO Auto-generated method stub
+			return 0;
 		}
 	}
-}
