a561297d71bfe76bb432d67734360ba80d4aaa1e
MARÍA ABASCAL <mariaabascal06@gmail.com>
2025-03-09 17:33:34 +0100

    Update Empleado.java

--- unidad3/solucionexamen/Empleado.java
+++ unidad3/solucionexamen/Empleado.java
@@ -1,37 +1,38 @@
-package unidad3.solucionexamen;
-
-/**
- * /**
- * Identificador, número
-nombre
-edad
-ingresos
-puesto
-
- */
+package unidad3.examenrepaso;
 
 public class Empleado {
-
+	/**************************************************************
+	 * VARIABLES
+	 *************************************************************/
+	//VARIABLES ESTATICAS
+	
 	public static final int PUESTO_JEFE = 0;
 	public static final int PUESTO_RRHH = 1;
 	public static final int PUESTO_ADMIN = 2;
 	public static final int PUESTO_VENTAS = 3;
-
+	
+	//Variable miembro
 	protected int id;
 	private String nombre;
 	private int edad;
 	private double ingresos;
 	public int puesto;
 
+	
+	/**************************************************************
+	 * CONSTRUCTORES
+	 *************************************************************/
+	
 	public Empleado() {
 		super();
-		this.id = (int) (Math.random() * 1000);
-		this.nombre = "";
-		this.edad = (int) (Math.random() * 50) + 18;
-		this.ingresos = (int) (Math.random() * 2000);
-		this.puesto = (int) (Math.random() * 4);
 		// TODO Auto-generated constructor stub
+		this.id = (int)(Math.random()*200);
+		this.nombre = "";
+		this.edad =(int)(Math.random()*50)+18 ;
+		this.ingresos = Math.random()*2000;
+		this.puesto = (int)(Math.random()*4);
 	}
+
 
 	public Empleado(int id, String nombre, int edad, double ingresos, int puesto) {
 		super();
@@ -42,43 +43,59 @@ public class Empleado {
 		this.puesto = puesto;
 	}
 
+
+	/**************************************************************
+	 * GETTERS Y SETTERS DE LA CLASE
+	 *************************************************************/
+
 	public int getId() {
 		return id;
 	}
+
 
 	public void setId(int id) {
 		this.id = id;
 	}
 
+
 	public String getNombre() {
 		return nombre;
 	}
+
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+
 	public int getEdad() {
 		return edad;
 	}
+
 
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
 
+
 	public double getIngresos() {
 		return ingresos;
 	}
 
+
 	public void setIngresos(double ingresos) {
 		this.ingresos = ingresos;
 	}
+	
+	/**************************************************************
+	 * TO STRING
+	 *************************************************************/
 
 	@Override
 	public String toString() {
-		return "Empleado:\n************************************************" + "id=" + id + "\n nombre=" + nombre
-				+ "\n edad=" + edad + "\n ingresos=" + ingresos + "\n puesto=" + puesto
-				+ "\n*************************************************************";
+		return "Empleado [id=" + id + ", nombre=" + nombre + ", edad=" + edad + ", ingresos=" + ingresos + ", puesto="
+				+ puesto + "]";
 	}
+	
 
 }
