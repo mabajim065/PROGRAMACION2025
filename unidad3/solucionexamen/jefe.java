8a47980699f8522bfa585e8cad46e2fcb425cc39
MARÍA ABASCAL <mariaabascal06@gmail.com>
2025-03-09 17:32:17 +0100

    Update jefe.java

--- unidad3/solucionexamen/jefe.java
+++ unidad3/solucionexamen/jefe.java
@@ -1,66 +1,74 @@
-package unidad3.solucionexamen;
+package unidad3.examenrepaso;
 
-/**
- * Esta clase es una clase hija de Empleado que hereda todas sus variables y funciones y además tiene sus atributos miembro:
+public class Jefe extends Empleado {
 
-gastoTarjeta
-consejo
-sede
-
- */
-public class jefe extends Empleado {
-
+	/**************************************************************
+	 * VARIABLES
+	 *************************************************************/
 	private double gastoTarjeta;
 	private String consejo;
 	private String sede;
+	
 
-	public jefe(int id, String nombre, int edad, double ingresos, int puesto, double gastoTarjeta, String consejo,
-			String sede) {
+	/**************************************************************
+	 * CONSTRUCTORES
+	 *************************************************************/
+	
+	public Jefe(int id, String nombre, int edad, double ingresos, int puesto) {
 		//llamamos al constructor de la clase padre que recibe todos las variables miembro
 		super(id, nombre, edad, ingresos, puesto);
-
+		// TODO Auto-generated constructor stub
+		this.gastoTarjeta = Math.random()*5000;
+		this.consejo = "";
+		this.sede = "";
+	}
+	
+	
+	public Jefe(double gastoTarjeta, String consejo, String sede) {
+		super();
 		this.gastoTarjeta = gastoTarjeta;
 		this.consejo = consejo;
 		this.sede = sede;
 	}
 
-	public jefe() {
-		super();
-
-		this.gastoTarjeta = 0;
-		this.consejo = "";
-		this.sede = "";
-		// TODO Auto-generated constructor stub
-	}
-
+	/**************************************************************
+	 * GETTERS Y SETTERS DE LA CLASE
+	 *************************************************************/
 	public double getGastoTarjeta() {
 		return gastoTarjeta;
 	}
+
 
 	public void setGastoTarjeta(double gastoTarjeta) {
 		this.gastoTarjeta = gastoTarjeta;
 	}
 
+
 	public String getConsejo() {
 		return consejo;
 	}
+
 
 	public void setConsejo(String consejo) {
 		this.consejo = consejo;
 	}
 
+
 	public String getSede() {
 		return sede;
 	}
+
 
 	public void setSede(String sede) {
 		this.sede = sede;
 	}
 
+	/**************************************************************
+	 * TO STRING
+	 *************************************************************/
 	@Override
 	public String toString() {
-		return "Jefe\n ******************************************************\n gastoTarjeta=" + gastoTarjeta
-				+ ", consejo=" + consejo + ", sede=" + sede + "]";
+		return "Jefe [gastoTarjeta=" + gastoTarjeta + ", consejo=" + consejo + ", sede=" + sede + "]";
 	}
-
+	
 }
